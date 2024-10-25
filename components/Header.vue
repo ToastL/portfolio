47f9aa5a3e18af4c3413b6a19512c1bb9b6e40d7
ToastL <etiendenouden2006@gmail.com>
2024-10-25 22:16:29 +0200

    [skip render] some bs

--- components/Header.vue
+++ components/Header.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import Logo from '~/assets/images/toaster.png'
 
 const graphql = useStrapiGraphQL()
@@ -14,11 +14,16 @@ const common = await graphql(`
     }
 `).then(res => res.data.common)
 
+const isMobile = ref<boolean>(false)
+onMounted(() => {
+  isMobile.value = window.matchMedia('(max-width: 600px)').matches
+})
+
 </script>
 
 <template>
     <header class="fixed flex justify-between items-center w-screen h-[6rem] backdrop-blur-md px-10">
-        <h1 class="text-xl"><img class="inline h-16" :src="useStrapiMedia(common.logo.url)" alt="Logo">{{ common.title }}</h1>
+        <a href="/"><h1 class="text-xl"><img class="inline h-16" :src="useStrapiMedia(common.logo.url)" alt="Logo"><span v-if="!isMobile">{{ common.title }}</span></h1></a>
         <div class="flex gap-5">
             <a class="text-md" href="#home">Home</a>
             <a class="text-md" href="#about">About</a>
